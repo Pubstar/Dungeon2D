f0f74359ae9d174e0cb925acb372e6f34ddda717
DiceOfDeath <DiceOfDeath@users.noreply.github.com>
2018-03-31 00:36:59 +0200

    BGM Handler
    Added easy BGM Handler

---
+++ Dungeon2D/Dungeon2D/BGMHandler.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon2D
+{
+    class BGMHandler : Game1
+    {
+        /// <summary>
+        /// This class loads up that lit background music fam
+        /// The way this class is designed is to allow you to call the class once. After which you can
+        /// Say you call this class as "BGM", from that moment on in the mainmodule you can do MediaPlayer.Play(BGM.SongName)
+        /// </summary>
+
+        public Song Dungeon2;
+
+        public void LoadSong()
+        {
+ //           Dungeon2 = Content.Load<Song>("BGM/Dungeon2");
+        }
+    }
+}
