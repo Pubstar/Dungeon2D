02934fa1c3a8419d247c8476706514795d14846e
Toez <tommydehoon@gmail.com>
2018-03-30 22:17:22 +0200

    Added World and Settings

---
+++ Dungeon2D/Dungeon2D/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon2D
+{
+    public static class Settings
+    {
+        public static int Width = 1280;
+        public static int Height = 720;
+    }
+}
---
+++ Dungeon2D/Dungeon2D/World.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeon2D
+{
+    class World
+    {
+        public World()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
